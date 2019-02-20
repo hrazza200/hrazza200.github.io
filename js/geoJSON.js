e718e487eecc8f6a5dc8b0144a34b3ab63b49256
hrazza200 <hrazza200@caledonian.ac.uk>
2019-02-20 20:19:50 +0000

    worked on nearby page

---
+++ js/geoJSON.js
@@ -0,0 +1,190 @@
+var geoJSON = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Hotel",
+        "icon": "/static/images/icons/map-marker-do.png",
+        "coordinates": [
+          -4.2625116,
+          55.8584792
+        ]
+      },
+      "properties": {
+        "name": "Radison_blu",
+        "description": "One of Europeâ€™s finest Gothic buildings, once the tallest building in the world that dominates Lincoln's skyline.",
+        "icon": "img/icons/hotel.png"
+      }
+    },
+
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Hotel",
+        "icon": "/static/images/icons/map-marker-do.png",
+        "coordinates": [
+          -4.2620562,
+          55.8640812
+        ]
+      },
+      "properties": {
+        "name": "Abode Glasgow Hotel",
+        "description": "A Beautiful Halal Hotel",
+        "icon": "img/icons/hotel.png"
+      }
+    },
+
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Hotel",
+        "icon": "/static/images/icons/map-marker-do.png",
+        "coordinates": [
+          -4.2891616,
+          55.8782545
+        ]
+      },
+      "properties": {
+        "name": "Argyll Western Hotel",
+        "description": "",
+        "icon": "img/icons/hotel.png"
+      }
+    },
+
+
+
+
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Attraction",
+        "icon": "img/icons/food.png",
+        "coordinates": [
+          -4.2507607,
+          55.8601314
+        ]
+      },
+      "properties": {
+        "name": "GOMA",
+        "description": "Gallery of Modern Art",
+        "icon": "img/icons/theater.png",
+        "link": "https://www.glasgowlife.org.uk/museums/venues/gallery-of-modern-art-goma"
+      }
+    },
+
+
+
+
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Attraction",
+          "icon": "img/icons/food.png",
+          "coordinates": [
+            -4.2927926,
+            55.8685891
+          ]
+        },
+        "properties": {
+          "name": "Kelvingrove",
+          "description": "Kelvingrove Art Gallery",
+          "icon": "img/icons/theater.png"
+        }
+      },
+
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Attraction",
+          "icon": "img/icons/food.png",
+          "coordinates": [
+            -4.2663669,
+            55.8638525
+          ]
+        },
+        "properties": {
+          "name": "Cineworld Cinema",
+          "description": "Cinema",
+          "icon": "img/icons/theater.png"
+        }
+      },
+
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Attraction",
+          "icon": "img/icons/food.png",
+          "coordinates": [
+            -4.27085896,
+            55.8650459
+          ]
+        },
+        "properties": {
+          "name": "Kings Theatre",
+          "description": "Theatre",
+          "icon": "img/icons/theater.png"
+        }
+      },
+
+
+
+
+
+
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Attraction",
+          "icon": "img/icons/theater.png",
+          "coordinates": [
+            -4.2663669,
+            55.8638525
+          ]
+        },
+        "properties": {
+          "name": "Cineworld Cinema",
+          "description": "Cinema",
+          "icon": "img/icons/theater.png"
+        }
+      },
+
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Restaurant",
+          "icon": "img/icons/food.png",
+          "coordinates": [
+            -4.2550565,
+            55.8655041
+          ]
+        },
+        "properties": {
+          "name": "The Flying Duck",
+          "description": "Restaurant",
+          "icon": "img/icons/food.png"
+        }
+      },
+
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Mosque",
+          "icon": "img/icons/mosque.png",
+          "coordinates": [
+            -4.2752521,
+            55.865215
+          ]
+        },
+        "properties": {
+          "name": "Berkeley Street Musallah",
+          "description": "Mosque",
+          "icon": "img/icons/mosque.png"
+        }
+      }
+
+
+
+
+  ]
+};
--- js/scripts.js
+++ js/scripts.js
@@ -1,117 +1,106 @@
 var map;
-var geoJSON = {
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "icon": "/static/images/icons/map-marker-do.png",
-        "coordinates": [
-          -4.2625116,
-          55.8584792
-        ]
-      },
-      "properties": {
-        "name": "Radison_blu",
-        "description": "One of Europeâ€™s finest Gothic buildings, once the tallest building in the world that dominates Lincoln's skyline.",
-        "icon": "img/icons/hotel.png"
-      }
-    },
-    {
-        "type": "Feature",
-        "geometry": {
-          "type": "Point",
-          "icon": "img/icons/food.png",
-          "coordinates": [
-            -4.2507607,
-            55.8601314
-          ]
-        },
-        "properties": {
-          "name": "GOMA",
-          "description": "Gallery of Modern Art",
-          "icon": "img/icons/theater.png"
-        }
-      },
-      {
-        "type": "Feature",
-        "geometry": {
-          "type": "Point",
-          "icon": "img/icons/food.png",
-          "coordinates": [
-            -4.2927926,
-            55.8685891
-          ]
-        },
-        "properties": {
-          "name": "Kelvingrove",
-          "description": "Kelvingrove Art Gallery",
-          "icon": "img/icons/theater.png"
-        }
-      }
-  ]
-};
+var markers = [];
+var latLong;
+var obj;
+var i = 0;
+var link;
 
-var Hotel1 = { lat: geoJSON["features"][0].geometry.coordinates[1], lng: geoJSON["features"][0].geometry.coordinates[0] };
-var GOMA = { lat: geoJSON["features"][1].geometry.coordinates[1], lng: geoJSON["features"][1].geometry.coordinates[0] };
-var Kelvingrove = { lat: geoJSON["features"][2].geometry.coordinates[1], lng: geoJSON["features"][2].geometry.coordinates[0] };
+// DrawMarkers on page load
+$( document ).ready(function() {
+  drawMarker();
+});
 
-
-
+// Just places the markers on the google maps
 function drawMarker() {
-  marker = new google.maps.Marker({
-    position: Hotel1, map: map, title: geoJSON["features"][0].properties.description,
-    icon: {
-      url: geoJSON["features"][0].properties.icon
-    }
-  });
 
-  google.maps.event.addListener(marker, 'click', function () {
-    findMarkersNearMe();
- });
-}
+  // loops through all elements in geoJSON
+  for (i; i < geoJSON.features.length; i++) {
 
-function findMarkersNearMe(){
+    // draw all of the hotels on the map
+    if (geoJSON.features[i].geometry.type == "Hotel") {
 
-    if(findUclidianDistance(GOMA["lat"],GOMA["lng"],Hotel1["lat"],Hotel1["lng"]) < 0.02){
-    var marker2 = new google.maps.Marker({
-        position: GOMA, map: map, title: geoJSON["features"][1].properties.description,
+      // gets the longitude and latitude of current element in geoJSON
+      latLong = getLatLong(geoJSON["features"][i].geometry.coordinates[1], geoJSON["features"][i].geometry.coordinates[0]);
+
+      // draws the marker on the map
+      markers[i] = new google.maps.Marker({
+        position: latLong, map: map, title: geoJSON["features"][i].properties.name,
         icon: {
-          url: geoJSON["features"][1].properties.icon
+          url: geoJSON["features"][i].properties.icon
         }
       });
-    
-      google.maps.event.addListener(marker2, 'click', function () {
-        alert("Take me to the website");
-     });
-    }
 
-    if(findUclidianDistance(Kelvingrove["lat"],Kelvingrove["lng"],Hotel1["lat"],Hotel1["lng"]) < 0.02){
-     var marker3 = new google.maps.Marker({
-        position: Kelvingrove, map: map, title: geoJSON["features"][2].properties.description,
-        icon: {
-          url: geoJSON["features"][2].properties.icon
+      // adds a function call on current marker when clicked
+      google.maps.event.addListener(markers[i], 'click', function (event) {
+        var l1,l2;
+        // get current marker longitude and latitude
+        for (var j = 0; j < geoJSON.features.length; j++) {
+          if (geoJSON.features[j].properties.name == this.title) {
+            l1 = geoJSON["features"][j].geometry.coordinates[1];
+            l2 = geoJSON["features"][j].geometry.coordinates[0];
+          }
         }
+        findMarkersNearMe(l1,l2);
       });
-    
-      google.maps.event.addListener(marker3, 'click', function () {
-        alert("Take me to the website");
-     });
-    } else {
-        alert("TOO FAR!");
     }
+  }
+
 }
 
-function findUclidianDistance(x1,y1,x2,y2){
-    var dist;
+// returns positision object
+function getLatLong(lat, long) {
+  pos = { lat: lat, lng: long }
+  return pos;
+}
 
-    var p1,p2;
+// draw al markers around the current marker given the longitude and latitude
+function findMarkersNearMe(lat1,lng1) {
 
-    p1 = Math.pow((x2 - x1),2);
-    p2 = Math.pow((y2 - y1),2);
+  var lat2, lng2;
 
-    dist = Math.sqrt(p1 + p2);
+  // loop through all the elements in the geoJSON
+  for (i = 0; i < geoJSON.features.length; i++) {
+    
+    // find all elements in geoJSON that are attractions or restaurants
+    if (geoJSON.features[i].geometry.type == "Attraction" || geoJSON.features[i].geometry.type == "Restaurant" || geoJSON.features[i].geometry.type == "Mosque") {
+      lat2 = geoJSON["features"][i].geometry.coordinates[1];
+      lng2 = geoJSON["features"][i].geometry.coordinates[0];
 
-    return dist;
+      if (findUclidianDistance(lat1, lng1, lat2, lng2) < 0.02) {
+        latLong = getLatLong(lat2,lng2);
+        markers[i] = new google.maps.Marker({
+          position: latLong, map: map, title: geoJSON["features"][i].properties.name,
+          icon: {
+            url: geoJSON["features"][i].properties.icon
+          }
+        });
+
+        google.maps.event.addListener(markers[i], 'click', function () {
+          for (var j = 0; j < geoJSON.features.length; j++) {
+            if (geoJSON.features[j].properties.name == this.title) {
+              link = geoJSON.features[j].properties.link;
+            }
+          }
+          alert(link);
+        });
+      }
+    }
+  }
+}
+
+// the distance between two points in the plane with coordinates (x, y) and (a, b)
+// use pythagorean theorum a^2 = b^2 + c^2
+// applied to distances, we do distance^2 = (a-x)^2 + (b-y)^2
+function findUclidianDistance(x1, y1, x2, y2) {
+ 
+  var p1, p2;
+
+  // p1 = (x2-x1)^2
+  p1 = Math.pow((x2 - x1), 2);
+
+  // p2 = (y1 -y2)^2
+  p2 = Math.pow((y2 - y1), 2);
+
+  // returns the sqaure root of the above two added together
+  return Math.sqrt(p1 + p2);
 }
