5bf37748d4d912ebaefce38a44a9cdbf017826f3
hrazza200 <hrazza200@caledonian.ac.uk>
2019-02-24 14:21:51 +0000

    updated geoJSON

--- js/geoJSON.js
+++ js/geoJSON.js
@@ -1,190 +1,544 @@
 var geoJSON = {
   "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Hotel",
-        "icon": "/static/images/icons/map-marker-do.png",
-        "coordinates": [
-          -4.2472867,
-          55.857262,
-        ]
-      },
-      "properties": {
-        "name": "Fraser Suites Glasgow",
-        "description": "One of Europeâ€™s finest Gothic buildings, once the tallest building in the world that dominates Lincoln's skyline.",
-        "icon": "img/icons/hotel.png"
-      }
+  "features": [{
+    "type": "Feature",
+    "geometry": {
+      "type": "Hotel",
+      "icon": "/static/images/icons/map-marker-do.png",
+      "coordinates": [
+        -4.2472867,
+        55.857262
+      ]
     },
+    "properties": {
+      "name": "Fraser Suites Glasgow",
+      "description": "One of Europeâ€™s finest Gothic buildings, once the tallest building in the world that dominates Lincoln's skyline.",
+      "icon": "img/icons/hotel.png"
+    }
+  },
 
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Hotel",
-        "icon": "/static/images/icons/map-marker-do.png",
-        "coordinates": [
-          -4.2620562,
-          55.8640812,
-        ]
-      },
-      "properties": {
-        "name": "Abode Glasgow Hotel",
-        "description": "A Beautiful Halal Hotel",
-        "icon": "img/icons/hotel.png"
-      }
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Hotel",
+      "icon": "/static/images/icons/map-marker-do.png",
+      "coordinates": [
+        -4.2620562,
+        55.8640812
+      ]
     },
+    "properties": {
+      "name": "Abode Glasgow Hotel",
+      "description": "A Beautiful Halal Hotel",
+      "icon": "img/icons/hotel.png"
+    }
+  },
 
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Hotel",
-        "icon": "/static/images/icons/map-marker-do.png",
-        "coordinates": [
-          -4.2886359,
-          55.8771321,
-        ]
-      },
-      "properties": {
-        "name": "Argyll Western Hotel",
-        "description": "",
-        "icon": "img/icons/hotel.png"
-      }
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Hotel",
+      "icon": "/static/images/icons/map-marker-do.png",
+      "coordinates": [
+        -4.2886359,
+        55.8771321
+      ]
     },
+    "properties": {
+      "name": "Argyll Western Hotel",
+      "description": "",
+      "icon": "img/icons/hotel.png"
+    }
+  },
 
 
 
 
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Attraction",
-        "icon": "img/icons/food.png",
-        "coordinates": [
-          -4.2507607,
-          55.8601314
-        ]
-      },
-      "properties": {
-        "name": "GOMA",
-        "description": "Gallery of Modern Art",
-        "icon": "img/icons/theater.png",
-        "link": "https://www.glasgowlife.org.uk/museums/venues/gallery-of-modern-art-goma"
-      }
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Attraction",
+      "icon": "img/icons/food.png",
+      "coordinates": [
+        -4.2507607,
+        55.8601314
+      ]
     },
+    "properties": {
+      "name": "GOMA",
+      "description": "Gallery of Modern Art",
+      "icon": "img/icons/theater.png",
+      "link": "https://www.glasgowlife.org.uk/museums/venues/gallery-of-modern-art-goma"
+    }
+  },
 
 
 
 
-      {
-        "type": "Feature",
-        "geometry": {
-          "type": "Attraction",
-          "icon": "img/icons/food.png",
-          "coordinates": [
-            -4.2927926,
-            55.8685891
-          ]
-        },
-        "properties": {
-          "name": "Kelvingrove",
-          "description": "Kelvingrove Art Gallery",
-          "icon": "img/icons/theater.png"
-        }
-      },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Attraction",
+      "icon": "img/icons/food.png",
+      "coordinates": [
+        -4.2927926,
+        55.8685891
+      ]
+    },
+    "properties": {
+      "name": "Kelvingrove",
+      "description": "Kelvingrove Art Gallery",
+      "icon": "img/icons/theater.png"
+    }
+  },
 
-      {
-        "type": "Feature",
-        "geometry": {
-          "type": "Attraction",
-          "icon": "img/icons/food.png",
-          "coordinates": [
-            -4.2663669,
-            55.8638525
-          ]
-        },
-        "properties": {
-          "name": "Cineworld Cinema",
-          "description": "Cinema",
-          "icon": "img/icons/theater.png"
-        }
-      },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Attraction",
+      "icon": "img/icons/food.png",
+      "coordinates": [
+        -4.2663669,
+        55.8638525
+      ]
+    },
+    "properties": {
+      "name": "Cineworld Cinema",
+      "description": "Cinema",
+      "icon": "img/icons/theater.png"
+    }
+  },
 
-      {
-        "type": "Feature",
-        "geometry": {
-          "type": "Attraction",
-          "icon": "img/icons/food.png",
-          "coordinates": [
-            -4.27085896,
-            55.8650459
-          ]
-        },
-        "properties": {
-          "name": "Kings Theatre",
-          "description": "Theatre",
-          "icon": "img/icons/theater.png"
-        }
-      },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Attraction",
+      "icon": "img/icons/food.png",
+      "coordinates": [
+        -4.27085896,
+        55.8650459
+      ]
+    },
+    "properties": {
+      "name": "Kings Theatre",
+      "description": "Theatre",
+      "icon": "img/icons/theater.png"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Restaurant",
+      "icon": "img/icons/food.png",
+      "coordinates": [
+        -4.2550565,
+        55.8655041
+      ]
+    },
+    "properties": {
+      "name": "The Flying Duck",
+      "description": "Restaurant",
+      "icon": "img/icons/food.png"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Mosque",
+      "icon": "img/icons/mosque.png",
+      "coordinates": [
+        -4.2752521,
+        55.865215
+      ]
+    },
+    "properties": {
+      "name": "Berkeley Street Musallah",
+      "description": "Mosque",
+      "icon": "img/icons/mosque.png"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Mosque",
+      "icon": "img/icons/mosque.png",
+      "coordinates": [
+        -4.2737093,
+        55.871642
+      ]
+    },
+    "properties": {
+      "name": "Al-Furqan Mosque",
+      "description": "Mosque",
+      "icon": "img/icons/mosque.png"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Mosque",
+      "icon": "img/icons/mosque.png",
+      "coordinates": [
+        -4.2533742,
+        55.852271,
+      ]
+    },
+    "properties": {
+      "name": "Glasgow Central Mosque",
+      "description": "Mosque",
+      "icon": "img/icons/mosque.png"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Mosque",
+      "icon": "img/icons/mosque.png",
+      "coordinates": [
+        -4.2922606,
+        55.8659491
+      ]
+    },
+    "properties": {
+      "name": "Baitur Rahman Mosque Glasgow",
+      "description": "Mosque",
+      "icon": "img/icons/mosque.png"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Mosque",
+      "icon": "img/icons/mosque.png",
+      "coordinates": [
+        -4.2721775,
+        55.8427984
+      ]
+    },
+    "properties": {
+      "name": "Masjid Noor",
+      "description": "Mosque",
+      "icon": "img/icons/mosque.png"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Mosque",
+      "icon": "img/icons/mosque.png",
+      "coordinates": [
+        -4.2865426,
+        55.8744432
+      ]
+    },
+    "properties": {
+      "name": "Dawatul Islam Glasgow",
+      "description": "Mosque",
+      "icon": "img/icons/mosque.png"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Mosque",
+      "icon": "img/icons/mosque.png",
+      "coordinates": [
+        -4.3193733,
+        55.8744894
+      ]
+    },
+    "properties": {
+      "name": "Ahl-al-Bayt Mosque",
+      "description": "Mosque",
+      "icon": "img/icons/mosque.png"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Mosque",
+      "icon": "img/icons/mosque.png",
+      "coordinates": [
+        -4.2617892,
+        55.8403536
+      ]
+    },
+    "properties": {
+      "name": "Masjid-E-Khazra",
+      "description": "Mosque",
+      "icon": "img/icons/mosque.png"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Restaurant",
+      "icon": "img/icons/food.png",
+      "coordinates": [
+        -4.2589317,
+        55.8637638
+      ]
+    },
+    "properties": {
+      "name": "Rishis Indian Aroma",
+      "description": "Restaurant",
+      "icon": "img/icons/food.png"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Restaurant",
+      "icon": "img/icons/food.png",
+      "coordinates": [
+        -4.2654978,
+        55.8637785
+      ]
+    },
+    "properties": {
+      "name": "Cook & Indi's World Buffet",
+      "description": "Restaurant",
+      "icon": "img/icons/food.png"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Restaurant",
+      "icon": "img/icons/food.png",
+      "coordinates": [
+        -4.2904349,
+        55.8771584
+      ]
+    },
+    "properties": {
+      "name": "Bread Meats Bread West End",
+      "description": "Restaurant",
+      "icon": "img/icons/food.png"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Restaurant",
+      "icon": "img/icons/food.png",
+      "coordinates": [
+        -4.285049,
+        55.8758525
+      ]
+    },
+    "properties": {
+      "name": "Brgr Glasgow Great Western Road",
+      "description": "Restaurant",
+      "icon": "img/icons/food.png"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Restaurant",
+      "icon": "img/icons/food.png",
+      "coordinates": [
+        -4.2939413,
+        55.8762294
+      ]
+    },
+    "properties": {
+      "name": "Cafe Andaluz West End",
+      "description": "Restaurant",
+      "icon": "img/icons/food.png"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Restaurant",
+      "icon": "img/icons/food.png",
+      "coordinates": [
+        -4.2456744,
+        55.8571464
+      ]
+    },
+    "properties": {
+      "name": "Obsession of India",
+      "description": "Restaurant",
+      "icon": "img/icons/food.png"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Restaurant",
+      "icon": "img/icons/food.png",
+      "coordinates": [
+        -4.2493084,
+        55.8550402
+      ]
+    },
+    "properties": {
+      "name": "Shawarma King",
+      "description": "Restaurant",
+      "icon": "img/icons/food.png"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Restaurant",
+      "icon": "img/icons/food.png",
+      "coordinates": [
+        -4.2477429,
+        55.8590548
+      ]
+    },
+    "properties": {
+      "name": "KoolBa",
+      "description": "Restaurant",
+      "icon": "img/icons/food.png"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Attraction",
+      "icon": "img/icons/food.png",
+      "coordinates": [
+        -4.2579127,
+        55.865085
+      ]
+    },
+    "properties": {
+      "name": "Pavilion Theatre",
+      "description": "Theatre",
+      "icon": "img/icons/theater.png"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Attraction",
+      "icon": "img/icons/food.png",
+      "coordinates": [
+        -4.2708596,
+        55.8650459
+      ]
+    },
+    "properties": {
+      "name": "King's Theatre",
+      "description": "Theatre",
+      "icon": "img/icons/theater.png"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Attraction",
+      "icon": "img/icons/food.png",
+      "coordinates": [
+        -4.2933664,
+        55.8797075
+      ]
+    },
+    "properties": {
+      "name": "Glasgow Botanic Gardens",
+      "description": "Garden",
+      "icon": "img/icons/theater.png"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Attraction",
+      "icon": "img/icons/food.png",
+      "coordinates": [
+        -4.2863473,
+        55.8792174
+      ]
+    },
+    "properties": {
+      "name": "The Sixty Steps",
+      "description": "Attraction",
+      "icon": "img/icons/theater.png"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Attraction",
+      "icon": "img/icons/food.png",
+      "coordinates": [
+        -4.2458002,
+        55.856798
+      ]
+    },
+    "properties": {
+      "name": "Merchant City Clock Tower",
+      "description": "Attraction",
+      "icon": "img/icons/theater.png"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Attraction",
+      "icon": "img/icons/food.png",
+      "coordinates": [
+        -4.2471007,
+        55.857987
+      ]
+    },
+    "properties": {
+      "name": "Glasgow Police Museum",
+      "description": "Attraction",
+      "icon": "img/icons/theater.png"
+    }
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Attraction",
+      "icon": "img/icons/food.png",
+      "coordinates": [
+        -4.2489899,
+        55.8570578
+      ]
+    },
+    "properties": {
+      "name": "Trongate 103",
+      "description": "Attraction",
+      "icon": "img/icons/theater.png"
+    }
+  }
 
 
 
-
-
-
-      {
-        "type": "Feature",
-        "geometry": {
-          "type": "Attraction",
-          "icon": "img/icons/theater.png",
-          "coordinates": [
-            -4.2663669,
-            55.8638525
-          ]
-        },
-        "properties": {
-          "name": "Cineworld Cinema",
-          "description": "Cinema",
-          "icon": "img/icons/theater.png"
-        }
-      },
-
-      {
-        "type": "Feature",
-        "geometry": {
-          "type": "Restaurant",
-          "icon": "img/icons/food.png",
-          "coordinates": [
-            -4.2550565,
-            55.8655041
-          ]
-        },
-        "properties": {
-          "name": "The Flying Duck",
-          "description": "Restaurant",
-          "icon": "img/icons/food.png"
-        }
-      },
-
-      {
-        "type": "Feature",
-        "geometry": {
-          "type": "Mosque",
-          "icon": "img/icons/mosque.png",
-          "coordinates": [
-            -4.2752521,
-            55.865215
-          ]
-        },
-        "properties": {
-          "name": "Berkeley Street Musallah",
-          "description": "Mosque",
-          "icon": "img/icons/mosque.png"
-        }
-      }
 
 
 
 
   ]
-};
+}
